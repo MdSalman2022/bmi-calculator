fa54e40dbeec70260ea201583d260dbfb7ce8cfd
MdSalman2022 <salman15-4523@diu.edu.bd>
2022-04-17 16:26:42 +0600

    Assignment

--- index.js
+++ index.js
@@ -8,20 +8,24 @@ btn.addEventListener("click", function(e){
     e.preventDefault();
     const totalheight = (height.value/100)*(height.value/100)
     const math = (weight.value/totalheight).toFixed(2)
-
-    result.innerHTML = `<p>Your BMI is: ${math}</p>`
+    if(height.value=="" || weight.value=="")
+    {
+        alert("Bhai kisu den")
+    }
+    else
+    result.innerHTML = `<h4>Your BMI is: ${math}</h4>`
    
     if(math<18.5){
-        advice.innerHTML = `<p> You are underweight</p>`
+        advice.innerHTML = `<h4> You are underweight</h4>`
     }
     else if(math>=18.5 && math<=24.9){
-        advice.innerHTML = `<p>You are Healthy</p>`
+        advice.innerHTML = `<h4>You are Healthy</h4>`
     }
     else if(math>=25 && math<=29.9){
-        advice.innerHTML = `<p>You are Overweight</p>`
+        advice.innerHTML = `<h4>You are Overweight</h4>`
     }
     else if(math>=30){
-        advice.innerHTML = `<p>You are Obese</p>`
+        advice.innerHTML = `<h4>You are Obese</h4>`
     }
     
 
